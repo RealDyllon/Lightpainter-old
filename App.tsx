ffdc2d34483a64387a71c350cd562ad6af43b3aa
Dyllon <dyllon.g@outlook.com>
2023-03-29 14:53:37 +0800

    clean up

--- App.tsx
+++ App.tsx
@@ -1,28 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect} from 'react';
 import {
   Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
 import {Base64, BleManager} from 'react-native-ble-plx';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 export const manager = new BleManager();
 
@@ -206,24 +193,5 @@ function App(): JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
