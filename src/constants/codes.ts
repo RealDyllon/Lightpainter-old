ea3cd825cb8815b1c42d9699d12d5ab24cf5282b
Dyllon <dyllon.g@outlook.com>
2023-04-05 20:02:23 +0800

    Fix strobing

--- src/constants/codes.ts
+++ src/constants/codes.ts
@@ -3,11 +3,11 @@ import { Base64 } from "react-native-ble-plx";
 export enum LightModes {
   Multicolor = "Multicolor",
   WarmWhite = "WarmWhite",
-  StaticColor = "StaticColor",
+  CustomColor = "CustomColor",
 }
 
 export const LightModesCodes: Record<LightModes, Base64> = {
   [LightModes.Multicolor]: "/gEABiABAAAAAA==",
   [LightModes.WarmWhite]: "/gEABiABAAABAA==",
-  [LightModes.StaticColor]: "/gEABiABAA3d0A==", //  todo: change this to match color picker state
+  [LightModes.CustomColor]: "/gEABiABAA3d0A==", //  todo: change this to match color picker state
 };
--- src/views/LightControl.tsx
+++ src/views/LightControl.tsx
@@ -8,12 +8,20 @@ import {
   View,
   ViewStyle,
 } from "react-native";
-import { Appbar, SegmentedButtons } from "react-native-paper";
-import { Slider } from "@miblanchard/react-native-slider";
-import React, { useState } from "react";
+import { Appbar, SegmentedButtons, Switch } from "react-native-paper";
+import React, { useEffect, useState } from "react";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
 import { LightModes, LightModesCodes } from "../constants/codes";
 import { writeData } from "../utils/writeData";
 import ColorWheel from "../components/ColorWheel";
+import BrightnessSlider from "../components/BrightnessSlider";
+import { delay } from "../utils/delay";
+import StrobeSlider from "../components/StrobeSlider";
 
 var Buffer = require("@craftzdog/react-native-buffer").Buffer;
 
@@ -25,8 +33,25 @@ const backgroundStyle: StyleProp<ViewStyle> = {
 const LightControl = () => {
   // const isDarkMode = useColorScheme() === 'dark';
 
+  const [powerState, setPowerState] = useState(true);
   const [lightMode, setLightMode] = useState<LightModes>(LightModes.Multicolor);
   const [brightness, setBrightness] = useState(20);
+  const [strobeFreq, setStrobeFreq] = useState(0);
+
+  const colorWheelAnimationStyle = useAnimatedStyle(() => {
+    return {
+      opacity: lightMode === LightModes.CustomColor ? 1 : 0,
+    };
+  });
+
+  const handlePowerStateChange = async (newValue: boolean) => {
+    setPowerState(newValue);
+    if (newValue) {
+      writeData("/gEAAwABAQ==");
+    } else {
+      writeData("/gEAAwABAA==");
+    }
+  };
 
   const handleLightModeChange = (value: string) => {
     setLightMode(value as LightModes);
@@ -45,6 +70,26 @@ const LightControl = () => {
     await writeData(payload);
   };
 
+  const handleStrobeFreqChange = async (value: number[]) => {
+    setStrobeFreq(value[0]);
+  };
+
+  useEffect(() => {
+    if (strobeFreq === 0) {
+      return;
+    }
+    const interval = setInterval(() => {
+      if (strobeFreq === 0) {
+        return;
+      }
+      writeData("/gEAAwABAA==");
+      delay((strobeFreq / 2) * 1000);
+      writeData("/gEAAwABAQ==");
+    }, strobeFreq * 1000);
+
+    return () => clearInterval(interval);
+  }, [strobeFreq]);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -59,61 +104,51 @@ const LightControl = () => {
         style={backgroundStyle}
       >
         <View>
-          <Text style={[styles.sectionHeader, { marginTop: 0 }]}>
-            Light Mode
-          </Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={[styles.sectionHeader, { marginTop: 0 }]}>
+              Light Mode
+            </Text>
+            <View style={{ flex: 1 }} />
+            <Switch
+              value={powerState}
+              onValueChange={handlePowerStateChange}
+              style={{ marginRight: 20, marginTop: 20 }}
+            />
+          </View>
           <SegmentedButtons
             value={lightMode}
             onValueChange={handleLightModeChange}
             buttons={Object.keys(LightModes).map((key) => ({
               value: key,
               label: key,
+              // showSelectedCheck: true,
             }))}
             style={{
               paddingHorizontal: 20,
             }}
           />
           <Text style={styles.sectionHeader}>Brightness</Text>
-          <View style={{ paddingHorizontal: 20 }}>
-            <Slider
-              value={brightness}
-              minimumValue={0}
-              maximumValue={100}
-              step={1}
-              onSlidingComplete={handleBrightnessChange}
-              // onValueChange={handleBrightnessChange}
-              trackStyle={{
-                height: 20,
-                borderRadius: 99,
-              }}
-              minimumTrackStyle={{
-                borderTopRightRadius: 0,
-                borderBottomRightRadius: 0,
-                backgroundColor: "#cccccc",
-              }}
-              renderThumbComponent={() => (
-                <View
-                  style={{
-                    width: 30,
-                    height: 30,
-                    borderRadius: 99,
-                    backgroundColor: "black",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <Text
-                    style={{ color: "white", fontWeight: "bold", fontSize: 18 }}
-                  >
-                    {brightness}
-                  </Text>
-                </View>
-              )}
-            />
-          </View>
-          <Text style={styles.sectionHeader}>Color</Text>
-          <ColorWheel />
+          <BrightnessSlider
+            brightness={brightness}
+            handleBrightnessChange={handleBrightnessChange}
+          />
+          <Text style={styles.sectionHeader}>Strobe Frequency</Text>
+          <StrobeSlider
+            frequency={strobeFreq}
+            handleFrequencyChange={handleStrobeFreqChange}
+          />
+          <Animated.View
+            entering={FadeIn}
+            exiting={FadeOut}
+            style={colorWheelAnimationStyle}
+          >
+            {lightMode === LightModes.CustomColor && (
+              <>
+                <Text style={styles.sectionHeader}>Color</Text>
+                <ColorWheel />
+              </>
+            )}
+          </Animated.View>
           <View />
         </View>
       </ScrollView>
