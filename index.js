f753f46360ce41836d313535512d41ed9f68b26f
Dyllon <dyllon.g@outlook.com>
2023-04-05 15:35:56 +0800

    add react native paper and update ui

--- index.js
+++ index.js
@@ -2,8 +2,30 @@
  * @format
  */
 
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
+import {
+  Provider as PaperProvider,
+  MD3LightTheme as DefaultTheme,
+} from 'react-native-paper';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    // primary: 'tomato',
+    // secondary: 'yellow',
+  },
+};
+
+function AppWithProviders() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => AppWithProviders);
